777f6ffe987c8783088a4f0d9f6fef3b87f2f590
Sahaban Arshad <Sahaban Arshad@Shaban>
2016-05-30 17:17:19 +0500

    hhhhh

---
+++ src/shaban/edu/pk/com/Subtraction.java
@@ -0,0 +1,15 @@
+package shaban.edu.pk.com;
+/**
+*@author Sahaban Arshad
+*@virsion 1.0 May 30, 2016
+*
+*/
+public class Subtraction {
+	public static void main(String[]arg)
+	{
+		int a=5;
+		int b=10;
+		System.out.println(a-b);
+	}
+
+}
